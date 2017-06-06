c8ef82989be93092719d9cdf9f87a49ec66ffc8a
Matt Flynn <matthew.g.flynn@gmail.com>
2017-06-06 20:11:09 +1000

    ractoring code

--- js/pixel.js
+++ js/pixel.js
@@ -2,8 +2,6 @@ function tableCreate(rows, cols){
     var myDiv = document.getElementById("pixel");
     tbl  = document.createElement('table');
 	tbl.setAttribute("id", "tbl_pixel");
-    //tbl.style.width  = '100px';
-    //tbl.style.border = '1px solid black';
 
     for(var i = 0; i < rows; i++){
         var tr = tbl.insertRow();
@@ -14,43 +12,46 @@ function tableCreate(rows, cols){
 			td.appendChild(document.createTextNode(''));
 			td.style.border = '1px solid black';
 			td.style.backgroundColor = 'white';
+			td.colorIdx = 0;
+			td.onclick = function () {tableText(this);};			
 			}
         }
     myDiv.appendChild(tbl);
 }
 
-tableCreate(8,8);
-var backgrounds = ["red","orange","yellow","green","lightblue","blue","pink","purple","white"];
-
-var table = document.getElementById("tbl_pixel");
-if (table != null) {
-    for (var i = 0; i < table.rows.length; i++) {
-        for (var j = 0; j < table.rows[i].cells.length; j++)
-        table.rows[i].cells[j].onclick = function () {
-            tableText(this);
-        };
-    }
+function tableText(myCell) {
+	myCell.colorIdx = myCell.colorIdx || 0;
+    myCell.style.backgroundColor = backgrounds[myCell.colorIdx++ % backgrounds.length];
+	updateJSON();
 }
 
-function tableText(el) {
-    //alert(tableCell.id);
-	//document.getElementById(tableCell.id).style.backgroundColor ="red";
-	el.colorIdx = el.colorIdx || 0;
-    el.style.backgroundColor = backgrounds[el.colorIdx++ % backgrounds.length];
-	exportJSON();
-}
-
-function exportJSON(){
+function updateJSON(){
 	if (table != null) {
 		colorMap = [];
 		for (var i = 0; i < table.rows.length; i++) {
 			for (var j = 0; j < table.rows[i].cells.length; j++){
 				//Retrive background color
 				//colorMap += table.rows[i].cells[j].style.backgroundColor;
-				cellColor = getComputedStyle(table.rows[i].cells[j], null).getPropertyValue("background-color").replace(/[^\d,]/g, '').split(',');
-				colorMap.push(" "+cellColor);
+				//cellColor = getComputedStyle(table.rows[i].cells[j], null).getPropertyValue("background-color").replace(/[^\d,]/g, '').split(',');
+				colorIndex = table.rows[i].cells[j].colorIdx
+				colorMap.push(" "+colorIndex);
 			}
 		}
-		document.getElementById("pixelJSON").innerHTML = colorMap;
+		document.getElementById("JSONdata").innerHTML = colorMap;
 	}
 }
+
+
+
+tableCreate(8,8);
+var backgrounds = ["red","orange","yellow","green","lightblue","blue","pink","purple","white"];
+var table = document.getElementById("tbl_pixel");
+updateJSON();
+//if (table != null) {
+//    for (var i = 0; i < table.rows.length; i++) {
+//        for (var j = 0; j < table.rows[i].cells.length; j++)
+//			table.rows[i].cells[j].onclick = function () {
+//				tableText(this);
+//			};		
+//    }
+//}
